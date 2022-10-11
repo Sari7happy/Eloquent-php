9d859cdfb135f02b49862045ca35ec7018b309b0
sari7happy <sari7happy@gmail.com>
2022-10-11 20:50:15 +0900

    削除機能実装

--- test-test/resources/views/contacts/show.blade.php
+++ test-test/resources/views/contacts/show.blade.php
@@ -70,20 +70,18 @@
                                             <div class="w-full  rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{$contact->contact}}</div>
                                         </div>
                                     </div>
-                                    <!-- チェック項目 -->
+                                    <!-- 編集 -->
                                         <form method="get" action="{{route('contacts.edit',['id' =>$contact->id])}}">
                                         <div class="p-2 w-full">
                                             <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</button>
                                         </div>
                                         </form>
 
-
-
-
+                                        <!-- 削除 -->
                                         <form id="delete_{{$contact->id}}" class="mt-40" method="post" action="{{route('contacts.destroy',['id' =>$contact->id])}}">
                                             @csrf
-                                        <div class="p-2 w-full">
-                                            <a  href="#" data-id="{{$contact->id}}" onclick = "deletePost(this)" class="flex mx-auto text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">削除する</a>
+                                        <div class="p-4 w-1/2">
+                                            <a href="#" data-id="{{$contact->id}}" onclick="deletePost(this)" class="flex mx-auto text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">削除する</a>
                                         </div>
                                         </form>
                                     </div>
@@ -101,7 +99,7 @@
         function deletePost(e){
             'use strict'
             if(confirm('本当に削除していいですか？')){
-                document.getElementById('delete_'+e.datyeset.id).submit()
+                document.getElementById('delete_'+e.dataset.id).submit()
             }
         }
     </script>
