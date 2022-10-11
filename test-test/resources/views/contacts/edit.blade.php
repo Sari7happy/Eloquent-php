6166186308b003b1f5be700f37f8a22a233ea56d
sari7happy <sari7happy@gmail.com>
2022-10-11 15:34:06 +0900

    更新のviewを表示

--- test-test/resources/views/contacts/edit.blade.php
+++ test-test/resources/views/contacts/edit.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <section class="text-gray-600 body-font relative">
-                        <form method="post" action="{{route('contacts.store')}}">
+                        <form method="post" action="{{route('contacts.update',['id'=>$contact->id])}}">
                             @csrf
                             <div class="container px-5 mx-auto">
 
@@ -79,10 +79,10 @@
                                             <textarea id="contact" name="contact" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{$contact->contact}}</textarea>
                                         </div>
                                     </div>
-                                    <!-- チェック項目 -->
+                                    <!-- ボタン項目 -->
                                     
                                         <div class="p-2 w-full">
-                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                         </div>
                                     </div>
                                 </div>
