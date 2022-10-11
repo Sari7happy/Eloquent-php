dc56e3992b3759a0efff44e24576b8d09d7f3986
sari7happy <sari7happy@gmail.com>
2022-10-11 14:44:01 +0900

    詳細画面から編集画面へリンク追加

--- test-test/resources/views/contacts/show.blade.php
+++ test-test/resources/views/contacts/show.blade.php
@@ -49,9 +49,9 @@
                                         <div class="p-2 w-full">
                                             <div class="relative">
                                                 <label class="leading-7 text-sm text-gray-600">性別</label><br>
-                                                <div class="w-full rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{$contact->gender}}</div>
+                                                <div class="w-full rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{$gender}}</div>
 
-                                                <br><br>
+                                                
                                             </div>
                                         </div>
                                     </div>
@@ -59,7 +59,7 @@
                                     <div class="p-2 w-full">
                                         <div class="relative">
                                             <label for="age" class="leading-7 text-sm text-gray-600">年齢</label>
-                                            <div class="w-full rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{$contact->age}}</div>
+                                            <div class="w-full rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">{{$age}}</div>
 
                                         </div>
                                     </div>
@@ -71,9 +71,11 @@
                                         </div>
                                     </div>
                                     <!-- チェック項目 -->
+                                        <form method="get" action="{{route('contacts.edit',['id' =>$contact->id])}}"
                                         <div class="p-2 w-full">
-                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">編集する</button>
                                         </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
