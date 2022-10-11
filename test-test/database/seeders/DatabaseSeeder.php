a23e90704eceff7e829359417467c720a30e60b1
sari7happy <sari7happy@gmail.com>
2022-10-12 00:05:23 +0900

    databeseseeder作成

--- test-test/database/seeders/DatabaseSeeder.php
+++ test-test/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TestSeeder::class,
+            UserSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 
